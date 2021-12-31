b0ec7b2e975ac75bd0fe3e205196f4ef645bec3e
kabarpos <kabarposmedia@gmail.com>
2021-12-31 08:05:45 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['public/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms') ],
+}
